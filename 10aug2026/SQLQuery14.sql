e871f87cfa4c019d5f26fb98a427f5a0b0b50de1
consult4best1 <consult4best1@gmail.com>
2026-08-25 12:35:23 +0500

    barqaab db backup
    barqaab db backup

--- 10aug2026/SQLQuery14.sql
+++ 10aug2026/SQLQuery14.sql
@@ -3,16 +3,36 @@ Equip_Type, pole_type,pole_use,pole_height,equip_use,
 Equip_Status,Transformer_Make,Transformer_Mounting,
 Transformer_Location,Con_Size_A, Con_Size_B, Con_Size_C, Con_Size_N,
 Remarks,
-select gps_no, x,y, con_size_a,con_size_b, con_size_c,con_size_n,
-pole_phase, 
-* from field_data
-where rs_con1 > 0
 
-select * from GPS_Points
+  --  '2250220196'
+
+select * from conductor where conductor_type = 'Feeder'
+
+--drop table field_data
+--select * into field_data from disco_barqaab.dbo.field_data_24aug
+
+select * from Pole_Type_Use
+
+insert into field_data(gps_no, x, y)
+select gps_num, x, y from gps_points
+
+insert into barqaab.dbo.field_data
+select * from disco_abdara.dbo.field_data
+
+--delete from drop table disco_barqaab.dbo.field_data 
+select * from disco_abdara.dbo.field_data
+alter table
+
+SELECT COUNT(*) AS total_columns
+FROM sys.columns --58
+WHERE object_id = OBJECT_ID('disco_barqaab.dbo.field_data');
+
+ 
 
 /*
-select distinct pole_use
+select distinct pole_phase, equip_type
 from field_data
+where equip_type in ('Secondary Pole', 'Transformer')
 
 select distinct equip_type
 from field_data
